b62f839dd28e8efb013527614a45c4f66c345c7c
XuLew <143716341+XuLew@users.noreply.github.com>
2024-05-06 16:55:55 +0800

    Update split_data.py

--- code/split_data.py
+++ code/split_data.py
@@ -3,9 +3,8 @@ import scipy.sparse as sp
 
 def splitData(dataY, splitPath, nfold, seed_cross, crossKey='cross'):
 
-    neg_pos_ratio = 1 #正负样本比例 1:1 1:5 1:10
+    neg_pos_ratio = 1 #5 #10
 
-    # 返回的是a=1对应的位置(二维数组)，需要用“行”(第0维)和“列”(第1维)表示
     index_pos = np.array(np.where(dataY == 1)) # (2,1923)
     index_neg = np.array(np.where(dataY == 0)) # (2,1068573)
 
@@ -13,16 +12,14 @@ def splitData(dataY, splitPath, nfold, seed_cross, crossKey='cross'):
     neg_num = int(pos_num * neg_pos_ratio)  # 1923
 
     np.random.seed(seed_cross)
-    np.random.shuffle(index_pos.T) # 多维矩阵中，shuffle只对第一维（行）做打乱顺序操作
+    np.random.shuffle(index_pos.T)
     np.random.seed(seed_cross)
     np.random.shuffle(index_neg.T)
     index_neg = index_neg[:, : neg_num]  # (2,1923)
 
-    # n折交叉检验，交叉检验的数据集cross_pos/neg下标对n求模 0,1,2,3,4
     cross_fold_index_pos = np.array([temp % nfold for temp in range(len(index_pos[0]))])
     cross_fold_index_neg = np.array([temp % nfold for temp in range(len(index_neg[0]))])
 
-    #第一轮，模为0的做测试集，其他做训练集；第二轮，1为测试集...
     for kfold in range(nfold):
         cross_tra_fold_pos = index_pos.T[cross_fold_index_pos != kfold]
         cross_tes_fold_pos = index_pos.T[cross_fold_index_pos == kfold]
